7aabab916a912331eabbec9cfea4c1b6daf97597
tiger <jizlin575@gmail.com>
2022-07-08 15:12:24 -0700

    cleared project, have db connection now.

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
-// const winston = require('winston');
 const express = require('express');
 const app = express();
 
-require('./startup/routes')(app);
 require('./startup/db')();
-require('./startup/config')();
-require('./startup/validation')();
-const logger = require('./startup/logging');
+// require('./startup/routes')(app);
+// require('./startup/config')();
+// require('./startup/validation')();
 
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
-  logger.info(`Listening on port ${port}...`)
+  console.log(`Listening on port ${port}...`)
 });
 module.exports = server;
--- startup/db.js
+++ startup/db.js
@@ -1,10 +1,9 @@
-const winston = require('winston');
 const mongoose = require('mongoose');
 const config = require('config');
-const logger = require('./logging');
+// const logger = require('./logging');
 
 module.exports = function() {
   const db = config.get('db');
   mongoose.connect(db)
-    .then(() => logger.info(`Connected to ${db}...`));
+    .then(() => console.log(`Connected to ${db}...`));
 }
